782175d9f90ddf2a2a68a861e16e89336ff06fe4
kbsx <k024024@outlook.com>
2023-11-03 10:53:56 -0500

    2023-11-3 commit

--- mean_variance_standard_deviation_calculator/mean_var_std.py
+++ mean_variance_standard_deviation_calculator/mean_var_std.py
@@ -5,32 +5,19 @@ list = [0,1,2,3,4,5,6,7,8]
 def calculate(list):
   if len(list) < 9:
     raise ValueError("List must contain nine numbers.")
-  arr = np.array(list)
-  reshaped = arr.reshape((3,3))
-  calculations = {
-      'mean' :  [
-        np.mean(reshaped, axis=0).tolist(), 
-        np.mean(reshaped, axis=1).tolist(), 
-        np.mean(reshaped, axis=None).tolist()],
-      'variance' : [
-        np.var(reshaped, axis=0).tolist(),
-        np.var(reshaped, axis=1).tolist(),
-        np.var(reshaped, axis=None).tolist()],
-      'standard deviation': [
-        np.std(reshaped, axis=0).tolist(),
-        np.std(reshaped, axis=1).tolist(),
-        np.std(reshaped, axis=None).tolist()],
-      'max': [
-        np.max(reshaped, axis=0).tolist(),
-        np.max(reshaped, axis=1).tolist(),
-        np.max(reshaped, axis=None).tolist()],
-      'min': [
-        np.min(reshaped, axis=0).tolist(),
-        np.min(reshaped, axis=1).tolist(),
-        np.min(reshaped, axis=None).tolist()],
-      'sum': [
-        np.sum(reshaped, axis=0).tolist(),
-        np.sum(reshaped, axis=1).tolist(),
-        np.sum(reshaped, axis=None).tolist()]
-  }
+  
+  arr = np.array(list).reshape((3,3))
+  
+  operations = ['mean', 'variance', 'standard deviation', 'max', 'min', 'sum']
+  np_operations = [np.mean, np.var, np.std, np.max, np.min, np.sum]
+  
+  calculations = {}
+  
+  for operation, np_operation in zip(operations, np_operations):
+    calculations[operation] = [
+      np_operation(arr, axis=0).tolist(),
+      np_operation(arr, axis=1).tolist(),
+      np_operation(arr, axis=None).tolist()
+    ]
+  
   return calculations
